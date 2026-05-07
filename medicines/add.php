909d354101c09ea1a483fa76a5686da0de409415
Harsh <harshdevops09@gmail.com>
2026-05-07 22:49:40 +0530

    final

--- medicines/add.php
+++ medicines/add.php
@@ -76,6 +76,8 @@ if(isset($_POST['add_medicine'])){
 
             <div class="row">
 
+                <div class="row">
+
     <div class="col-md-6">
 
         <label>Medicine Name</label>
@@ -161,64 +163,6 @@ if(isset($_POST['add_medicine'])){
     </div>
 
 </div>
-
-                    <input type="text"
-                           name="medicine_name"
-                           class="form-control"
-                           required>
-
-                </div>
-
-                <div class="col-md-6 mb-3">
-
-                    <label class="mb-2">Stock Quantity</label>
-
-                    <input type="number"
-                           name="stock_quantity"
-                           class="form-control"
-                           required>
-
-                </div>
-
-            </div>
-
-            <div class="row">
-
-                <div class="col-md-4 mb-3">
-
-                    <label class="mb-2">Expiry Date</label>
-
-                    <input type="date"
-                           name="expiry_date"
-                           class="form-control"
-                           required>
-
-                </div>
-
-                <div class="col-md-4 mb-3">
-
-                    <label class="mb-2">Refill Days</label>
-
-                    <input type="number"
-                           name="refill_days"
-                           class="form-control"
-                           required>
-
-                </div>
-
-                <div class="col-md-4 mb-3">
-
-                    <label class="mb-2">Price</label>
-
-                    <input type="number"
-                           step="0.01"
-                           name="price"
-                           class="form-control"
-                           required>
-
-                </div>
-
-            </div>
 
             <button type="submit"
                     name="add_medicine"
